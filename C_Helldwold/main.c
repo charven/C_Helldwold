e195f509488a05affee9497d71712504f08a86cb
bb <charvenhe@8000mdt-server.tencent.com>
2014-09-22 23:39:25 +0800

    Initial Commit

---
+++ C_Helldwold/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C_Helldwold
+//
+//  Created by bb on 14-9-22.
+//  Copyright (c) 2014年 bb. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
